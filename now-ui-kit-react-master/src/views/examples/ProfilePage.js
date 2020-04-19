8163071c1c4db7a3f401e272ac633b23745456c4
Oliver Skeates <56803202+OliSkeates@users.noreply.github.com>
2020-04-19 16:43:35 +0100

    Update ProfilePage.js

--- now-ui-kit-react-master/src/views/examples/ProfilePage.js
+++ now-ui-kit-react-master/src/views/examples/ProfilePage.js
@@ -2,14 +2,11 @@ import React from "react";
 
 // reactstrap components
 import {
-  Button,
-  Container,
-  UncontrolledTooltip
+  Container
 } from "reactstrap";
 
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
-import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
 import DefaultFooter from "components/Footers/DefaultFooter.js";
 import Mqtt from "./MQTTclient.js";
 
@@ -29,45 +26,16 @@ function ProfilePage(state) {
 
   return (
     <>
+    <div
+      className="background"
+      style={{backgroundImage: "url(" + require("assets/img/blue.jpg") + ")"}}
+    >
+    <Container>
       <ExamplesNavbar />
-      <div className="wrapper">
-        <ProfilePageHeader />
-        <div className="section">
-          <Container>
-            <div className="button-container">
-              <Button className="btn-round" color="info" size="lg">
-                Follow
-              </Button>
-              <Button
-                className="btn-round btn-icon"
-                color="default"
-                id="tooltip515203352"
-                size="lg"
-              >
-                <i className="fab fa-twitter"></i>
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip515203352">
-                Follow me on Twitter
-              </UncontrolledTooltip>
-              <Button
-                className="btn-round btn-icon"
-                color="default"
-                id="tooltip340339231"
-                size="lg"
-              >
-                <i className="fab fa-instagram"></i>
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip340339231">
-                Follow me on Instagram
-              </UncontrolledTooltip>
-            </div>
 
           <Mqtt type = "profile" />
-
-
-          </Container>
-        </div>
         <DefaultFooter />
+      </Container>
       </div>
     </>
   );
